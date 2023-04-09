a20bb4596933b302a6ea8ebb61385b27f2ca745b
Saba <sababolourinovin@gmail.com>
2023-04-09 10:28:28 +0430

    handle error

--- pages/api/posts/[postId].js
+++ pages/api/posts/[postId].js
@@ -1,12 +1,13 @@
-import data from "../data";
+import data from "@/data";
 
 export default function handler(req, res) {
     const { postId } = req.query;
     const { Posts } = data;
 
     if (postId) {
-        const post = Posts.find(value => value.id == postId);
+        const post = Posts.find((value) => value.id == postId);
         res.status(200).json(post);
     }
-    return res.status(404).json({ error: 'Data not found!' });
+
+    return res.status(404).json({ error: "Data not found!" });
 }
